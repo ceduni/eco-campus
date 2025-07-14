f2671d266ea44fddbefef0fe675d327adc269029
Caty12 <catina.beaulieu@umontreal.ca>
2025-07-14 11:44:07 -0400

    Layers implémentation

--- template-projet-main-3/eco-campus/frontend/src/components/Layers.jsx
+++ template-projet-main-3/eco-campus/frontend/src/components/Layers.jsx
@@ -5,23 +5,24 @@ const Layers = () => {
   return (
     <div className={styles.layersBox}>
       <h3 className={styles.title}>Les couches</h3>
-      <div className={styles.iconsGrid}>
-        <div className={styles.iconItem}>
+      <div className={styles.gridContainer}>
+        <button className={styles.iconItem}>
           <img src="/src/assets/stars.svg" alt="Stars" />
           <span>Stars</span>
-        </div>
-        <div className={styles.iconItem}>
-          <img src="/src/assets/zones.svg" alt="Zones vertes" />
-          <span>Zones vertes vs. Non-vertes</span>
-        </div>
-        <div className={styles.iconItem}>
+        </button>
+        <button className={styles.iconItem}>
+          <img src="/src/assets/zone.svg" alt="Zones" />
+          <span>Zones&nbsp;vertes<br />vs.&nbsp;Non-vertes</span>
+
+        </button>
+        <button className={styles.iconItem}>
           <img src="/src/assets/universite.svg" alt="Université" />
           <span>Université</span>
-        </div>
-        <div className={styles.iconItem}>
+        </button>
+        <button className={styles.iconItem} disabled>
           <img src="/src/assets/pavillon.svg" alt="Pavillon" />
           <span>Pavillon</span>
-        </div>
+        </button>
       </div>
     </div>
   );
