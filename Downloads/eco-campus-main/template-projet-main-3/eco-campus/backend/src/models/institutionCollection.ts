5996b3fe9222adb4bc8b0071289ed7f03b87700a
Asmaa Skou <91071080+AsmaaZohra@users.noreply.github.com>
2025-07-18 14:09:13 -0400

    Delete Downloads/eco-campus-main/template-projet-main-3/eco-campus directory

--- Downloads/eco-campus-main/template-projet-main-3/eco-campus/backend/src/models/institutionCollection.ts
+++
@@ -1,9 +0,0 @@
-import { institutionData } from './institutionData';
-
-export class institutionCollection {
-  institutions: institutionData[];
-
-  constructor(institutions: institutionData[] = []) {
-    this.institutions = institutions;
-  }
-}
