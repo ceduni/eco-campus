6c7c7d206a0a78e17a82092521dccc5269d35d63
Asmaa Skou <91071080+AsmaaZohra@users.noreply.github.com>
2025-07-29 17:12:18 -0400

    Update ComponentTesting.jsx

--- template-projet-main-3/eco-campus/frontend/src/pages/ComponentTesting.jsx
+++ template-projet-main-3/eco-campus/frontend/src/pages/ComponentTesting.jsx
@@ -1,12 +1,12 @@
 import Map from '../components/Map';
 import React from 'react';
 import { MetricPanel } from '../components/MetricPanel';
+import UniversityComparisonPanel from '../components/ComparaisonPanel';
 
 export function ComponentTesting() {
   return (
     <div>
-        <h1>Eco-Campus : component testing</h1>
-        <MetricPanel/>
+        <UniversityComparisonPanel idA="1" idB="2" /> {/* test , les id devront etre prit dynamiquement selon la selection de l'utilisateur ...*/}
     </div>
   );
 }
